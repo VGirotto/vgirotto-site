75a415632984c559b94f833df8cc297b02ce574c
Vinícius <vinicius.victor.girotto@gmail.com>
2020-12-12 02:38:09 -0300

    Reformulacao

---
+++ pages/drop_enemies.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vinicius Girotto - Drop RPG</title>
+  <link rel="stylesheet" type="text/css" href="../styles/gameStyle.css" />
+
+</head>
+<body>
+
+    <div class="container" >
+        <div class="navbar">
+            <a href="../index.php"> Início</a>
+            <div class="dropdown">
+                <button class="dropbtn">Drop RPG</button>
+                <div class="dropdown-content">
+                    <a href="droprpg.php">Game</a>
+                    <a class="active"href="drop_enemies.php">Enemies</a>
+                    <a href="#">Items</a>
+                </div>
+            </div>
+            <a href="sobre.php"> Sobre</a>
+        </div>
+    </div> 
+
+
+    <!--Título-->
+    <div class="content1">
+        <div>
+            <h1>Drop RPG</h1>          
+        </div>
+    </div>
+
+
+    <div class="content2">
+        <div id="main">
+            
+            <h2 class = "center"> Bosses </h2>
+
+            <br /> 
+            
+            <table class = "center" style="width:100%">
+                <tr>
+                    <th>Viking</th>
+                    <th colspan="7">Status</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="../resources/enemies/Viking.png" alt="Viking image"></td>
+                    <th>HP</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Critical</th>
+                    <th>Wisdom</th>
+                    <th>Resistance</th>
+                </tr>
+                <tr>
+                    <td>200</td>
+                    <td>30</td>
+                    <td>20</td>
+                    <td>10</td>
+                    <td>5</td>
+                    <td>100</td>
+                    <td>10</td>
+                </tr>
+                </table>
+            <br />
+
+            <table class = "center" style="width:100%">
+                <tr>
+                    <th>Manticore</th>
+                    <th colspan="7">Status</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="../resources/enemies/Manticore.png" alt="Manticore image"></td>
+                    <th>HP</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Critical</th>
+                    <th>Wisdom</th>
+                    <th>Resistance</th>
+                </tr>
+                <tr>
+                    <td>200</td>
+                    <td>80</td>
+                    <td>60</td>
+                    <td>30</td>
+                    <td>1</td>
+                    <td>200</td>
+                    <td>5</td>
+                </tr>
+                </table>
+            <br /><br /><br />
+
+            <h2 class = "center"> Enemies </h2>
+            <br />
+            <table class = "center" style="width:100%">
+                <tr>
+                    <th>Wild Boar</th>
+                    <th colspan="7">Status</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="../resources/enemies/Wild_boar.png" alt="Wild Boar image"></td>
+                    <th>HP</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Critical</th>
+                    <th>Wisdom</th>
+                    <th>Resistance</th>
+                </tr>
+                <tr>
+                    <td>100</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>5</td>
+                    <td>5</td>
+                    <td>10</td>
+                    <td>20</td>
+                </tr>
+                </table>
+            <br />
+
+            <table class = "center" style="width:100%">
+                <tr>
+                    <th>Ancient</th>
+                    <th colspan="7">Status</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="../resources/enemies/Ancient.png" alt="Ancient image"></td>
+                    <th>HP</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Critical</th>
+                    <th>Wisdom</th>
+                    <th>Resistance</th>
+                </tr>
+                <tr>
+                    <td>100</td>
+                    <td>20</td>
+                    <td>10</td>
+                    <td>10</td>
+                    <td>3.5</td>
+                    <td>10</td>
+                    <td>20</td>
+                </tr>
+                </table>
+            <br />
+
+            <table class = "center" style="width:100%">
+                <tr>
+                    <th>Anubis</th>
+                    <th colspan="7">Status</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="../resources/enemies/Anubis.png" alt="Anubis image"></td>
+                    <th>HP</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Critical</th>
+                    <th>Wisdom</th>
+                    <th>Resistance</th>
+                </tr>
+                <tr>
+                    <td>100</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>30</td>
+                    <td>5</td>
+                    <td>30</td>
+                    <td>20</td>
+                </tr>
+                </table>
+            <br />
+
+            <table class = "center" style="width:100%">
+                <tr>
+                    <th>Revived Statue</th>
+                    <th colspan="7">Status</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="../resources/enemies/Revived_statue.png" alt="Revived Statue image"></td>
+                    <th>HP</th>
+                    <th>Attack</th>
+                    <th>Defense</th>
+                    <th>Agility</th>
+                    <th>Critical</th>
+                    <th>Wisdom</th>
+                    <th>Resistance</th>
+                </tr>
+                <tr>
+                    <td>120</td>
+                    <td>40</td>
+                    <td>50</td>
+                    <td>15</td>
+                    <td>10</td>
+                    <td>30</td>
+                    <td>20</td>
+                </tr>
+                </table>
+            <br />
+    </div> 
+</body>
+</html>
---
+++ pages/droprpg.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vinicius Girotto - Drop RPG</title>
+  <link rel="stylesheet" type="text/css" href="../styles/gameStyle.css" />
+
+</head>
+<body>
+
+    <div class="container" >
+        <div class="navbar">
+            <a href="../index.php"> Início</a>
+            <div class="dropdown">
+                <button class="dropbtn">Drop RPG</button>
+                <div class="dropdown-content">
+                    <a class="active" href="droprpg.php">Game</a>
+                    <a href="drop_enemies.php">Enemies</a>
+                    <a href="#">Items</a>
+                </div>
+            </div>
+            <a href="sobre.php"> Sobre</a>
+        </div>
+    </div> 
+
+
+    <!--Título-->
+    <div class="content1">
+        <div>
+            <h1>Drop RPG</h1>          
+        </div>
+    </div>
+
+
+    <div class="content2">
+        <div id="main">
+            <p class= "center"> Clique no play para jogar. <br />
+            <iframe src="https://itch.io/embed/768042" height="167" width="600" frameborder="0"><a href="https://vgirotto.itch.io/drop-rpg">Drop RPG by VGirotto</a></iframe> </p>
+            <br />
+            <p class="left">
+            Drop RPG is an <b>idle RPG game</b> for you test your <b>luck</b> and your <b>strategy</b>.
+            <br /><br />
+            <b>Features:</b>
+            <br /><br />
+            <ul>
+                <li><b>Every battle you get an item;</b> </li>
+                <li>The <b>items can stack until 5</b>. For each additional item of the same name it gains more 10% of the stats; </li>
+                <li><b>After every battle you lost 10 of your max HP</b>, this is the challenge! </li>
+                <li>Each map has a <b>boss</b>, you have to defeat him to go to the next map; </li>
+                <li>When you died, you can <b>restart with all your equipaments</b>, but level 1 again (there and back); </li>
+                <li>Different <b>statuses</b> to vary battles; </li>
+                <li>Your game is <b>constantly saved</b>; </li>
+                <li>and more, play and you will see ;) </li>
+            </ul>
+            <br />
+            <br />
+            The game is <b>not completed</b> and has only 2 bosses, unfortunately I didn't have time to finish.
+            <br />
+            <br />
+            <br />
+            <b> Credits: </b>
+            <br />
+            <ul>
+                <li>All the assets are from CraftPix (I have a subscription); </li>
+                <li>The soundtracks are from https://stephanef.itch.io/music-for-roleplaying-vol1; </li>
+                <li>Sound of attack by qubodup. </li>
+            </ul>
+        </div>
+    
+    </div> 
+</body>
+</html>
